817779c0d8bbdfc914da641b3b45301a186761b9
kiryanchi <kiryanchi@gmail.com>
2024-04-15 21:01:35 +0900

    :sparkles: Load Cogs

--- sky_whale/cog/music_cog.py
+++ sky_whale/cog/music_cog.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
-from discord.ext.commands import GroupCog
+
+from discord import app_commands
+from discord.ext.commands import GroupCog, Cog
 
 if TYPE_CHECKING:
+    from discord import Message, Interaction
     from sky_whale.extended_bot import ExtendedBot
 
 
@@ -10,6 +13,13 @@ class MusicCog(GroupCog, name="고래"):
 
     def __init__(self, bot: ExtendedBot):
         self.bot = bot
+
+    @Cog.listener()
+    async def on_message(self, message: Message) -> None:
+        if message.author.bot:
+            return
+
+        # search music in YouTube
 
 
 async def setup(bot: ExtendedBot) -> None:
--- sky_whale/extended_bot.py
+++ sky_whale/extended_bot.py
@@ -1,4 +1,7 @@
-from discord import Intents
+import glob
+import os
+
+from discord import Intents, Object
 from discord.ext.commands import Bot
 
 
@@ -7,5 +10,19 @@ class ExtendedBot(Bot):
     def __init__(self) -> None:
         super().__init__(intents=Intents.all(), command_prefix="!")
 
+    async def setup_hook(self) -> None:
+        await self._load_cogs()
+        await self._sync_cogs()
+
     def on_ready(self) -> None:
         print(f"Logged in as {self.user}")
+
+    async def _load_cogs(self) -> None:
+        for cog in glob.glob("sky_whale/cog/*.py"):
+            cog = cog[:-3].replace("/", ".")
+            await self.load_extension(cog)
+
+    async def _sync_cogs(self) -> None:
+        admin_guild = Object(os.environ.get("ADMIN_GUILD_ID"))
+        self.tree.copy_global_to(guild=admin_guild)
+        await self.tree.sync()
