eeb6366b830babbaacaf08217dba3318363315b4
kiryanchi <kiryanchi@gmail.com>
2024-04-17 19:51:43 +0900

    remove load dotenv from base db

--- sky_whale/db/base.py
+++ sky_whale/db/base.py
@@ -4,8 +4,6 @@ from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase
 
-load_dotenv()
-
 db_path = os.environ.get("DB_PATH")
 engine = create_async_engine(f"sqlite+aiosqlite://{db_path}", echo=False)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
