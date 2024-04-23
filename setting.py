6d32bbed9a7a3e3543dad42bf77a67e79e6dd729
kiryanchi <kiryanchi@naver.com>
2024-04-23 15:53:06 +0000

    🚑️ Hotfix: INIT MSG

--- setting.py
+++ setting.py
@@ -9,4 +9,5 @@ DEFAULT_DELETE_COLOR: Final[int] = 0xFF3B56
 CHANNEL_NAME = "🐳ㆍ하늘-고래"
 DEFAULT_DISCORD_INVITE_URL = "https://discord.com/api/oauth2/authorize?client_id=965786057897541682&permissions=8&scope=bot%20applications.commands"
 DEFAULT_DISCORD_GUILD_URL = "https://discord.gg/T92wcQuznv"
-INIT_MSG = "```ansi[2;34m[0m[2;32m[2;36m하늘 고래[0m[2;32m[0m[2;34m[0m[2;37m가[0m [2;34m하늘[0m[2;37m을[0m [2;31m향유[0m[2;37m하기 시작했어요.[0m```"
+INIT_MSG = '''```ansi
+[2;34m[0m[2;32m[2;36m하늘 고래[0m[2;32m[0m[2;34m[0m[2;37m가[0m [2;34m하늘[0m[2;37m을[0m [2;31m향유[0m[2;37m하기 시작했어요.[0m```'''
