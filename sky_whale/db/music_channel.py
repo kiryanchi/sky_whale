1df16b5279a8c718afa9ac725994b0d479e9fc29
kiryanchi <kiryanchi@gmail.com>
2024-05-26 16:19:09 +0900

    sqlite3

--- sky_whale/db/music_channel.py
+++ sky_whale/db/music_channel.py
@@ -3,15 +3,19 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
+from sqlalchemy import Column, Integer
+from sqlalchemy.orm import DeclarativeBase
+
 if TYPE_CHECKING:
-    from sky_whale.dto.music_channel_dto import MusicChannelDto
+    pass
+
+
+class Base(DeclarativeBase):
+    pass
 
 
 @dataclass(frozen=True)
-class MusicChannel:
-    guild_id: int
-    channel_id: int
-
-    @classmethod
-    def from_dto(cls, dto: MusicChannelDto):
-        return cls(guild_id=dto["guild_id"], channel_id=dto["channel_id"])
+class MusicChannel(Base):
+    __tablename__ = "music_channel"
+    guild_id = Column(Integer, primary_key=True)
+    channel_id = Column(Integer)
